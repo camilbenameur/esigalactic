97f125eb8e67c1c4f6b3be16ba2b57edd396b22b
G3R74W <83832635+G3R74W@users.noreply.github.com>
2023-05-01 15:24:10 +0200

    Add files via upload

--- add-user.php
+++ add-user.php
@@ -6,15 +6,23 @@ if(isset($_POST['name']) && isset($_POST['password']) && isset($_POST['mail']))
     $name = $_POST['name'];
     $password = $_POST['password'];
     $mail = $_POST['mail'];
-    $query = "INSERT INTO users (id, name, password, mail) VALUES (NULL, :name, :password, :mail)";
-    $stmt = $db->prepare($query);
-    $stmt->bindParam(':name', $name);
-    $stmt->bindParam(':password', $password);
-    $stmt->bindParam(':mail', $mail);
-    if ($stmt->execute()) {
-        echo "Les données ont été ajoutées avec succès.";
-    } else {
-        $error = $stmt->errorInfo();
-        echo "Erreur : " . $error[2];
-    }
+    $query = $db->prepare("SELECT id FROM users WHERE name = ? OR mail = ?;");
+    $query->execute([$name, $mail]);
+    $rows = $query->fetchAll();
+    if(count($rows)>0){
+        echo "username or e-mail already used ";
+   }
+   else{
+        $query = "INSERT INTO users (id, name, password, mail) VALUES (NULL, :name, :password, :mail)";
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':mail', $mail);
+        if ($stmt->execute()) {
+            echo "Les données ont été ajoutées avec succès.";
+        } else {
+            $error = $stmt->errorInfo();
+            echo "Erreur : " . $error[2];
+        }
+   }
 }
