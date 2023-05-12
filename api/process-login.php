6e17a46292c1260abd6c85a3084ba680494f53c0
camilbenameur <benameurcamil@gamil.com>
2023-05-12 20:57:06 +0200

    moved to api folder

---
+++ api/process-login.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+
+$db = new PDO("mysql:host=localhost;dbname=esigalactic","root", "");
+if(isset($_POST['name']) && isset($_POST['password']) && isset($_POST['mail']))
+{
+ $name = $_POST['name'];
+ $password = $_POST['password'];
+ $hash_password = hash("sha512", $password);
+ $mail = $_POST['mail'];
+ $query = $db->prepare("SELECT id FROM player WHERE name = ? AND password = ? AND mail = ?;");
+ $query->execute([$name, $hash_password, $mail]);
+ $rows = $query->fetchAll();
+ if(count($rows)>0){
+    echo "Identifiants corrects";
+    $_SESSION["connected"] = true;
+    $_SESSION["universe"] = $_POST['universe-choice'];
+    header("Location:galaxy.php");
+}
+ else{
+    echo "Identifiants incorrects";
+    header("Location:login.html");
+}
+}
+else
+{
+ echo "Requête incorrecte";
+}
