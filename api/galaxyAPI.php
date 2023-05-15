7b103cb16feb2ae501496ebb8d59e06132d466e2
camilbenameur <benameurcamil@gamil.com>
2023-05-15 21:55:05 +0200

    join attempt but failed

--- api/galaxyAPI.php
+++ api/galaxyAPI.php
@@ -3,10 +3,14 @@ $_SESSION["galaxy-choice"] = $_GET["galaxy-choice"];
 $_SESSION["solar-system-choice"] = $_GET["solar-system-choice"];
 
 $db = new PDO("mysql:host=localhost;dbname=esigalactic","root", "");
-$query = $db->prepare("SELECT * FROM planet WHERE ? = solar_system_id;");
-$query->execute([$_SESSION["solar-system-choice"]]);
-$rows = $query->fetchAll();
+$query = $db->prepare(" SELECT p.*
+FROM planet p
+JOIN solar_system ss ON p.solar_system_id = ss.id
+WHERE ss.id = ? AND ss.galaxy_id = ?;
 
+");
+$query->execute([$_SESSION["solar-system-choice"] ,$_SESSION["galaxy-choice"]]);
+$rows = $query->fetchAll();
 echo json_encode($rows);
 
 ?>
