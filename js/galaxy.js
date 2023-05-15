3011cfd42bba87102912130d1bc30d25c349c5ed
camilbenameur <benameurcamil@gamil.com>
2023-05-15 22:00:43 +0200

    now display the solar system

--- js/galaxy.js
+++ js/galaxy.js
@@ -1,20 +1,26 @@
-const url = 'http://esigalactic/api/galaxyAPI.php';
-const galaxyChoice = document.getElementById('galaxy-choice');
-const solarSystemChoice = document.getElementById('solar-system-choice');
-
-
-
+let url = 'http://esigalactic/api/galaxyAPI.php';
+let galaxyChoice = document.getElementById('galaxy-choice');
+let solarSystemChoice = document.getElementById('solar-system-choice');
+let planetDisplay = document.getElementById('planet-display');
 
 async function update() {
-  let galaxyId = galaxyChoice.value;
-  let solarSystemId = solarSystemChoice.value;
+  planetDisplay.innerHTML = '';
+  const galaxyId = galaxyChoice.value;
+  const solarSystemId = solarSystemChoice.value;
   const answer = await fetch(url + '?galaxy-choice=' + galaxyId + '&solar-system-choice=' + solarSystemId);
   const data = await answer.json();
   data.forEach(element => {
-    console.log(element);
-  });
-  
+
+    let planet = document.createElement('p');
+    planet.id = 'planet-' + element.id;
+    planet.classList.add('planet');
+    planet.innerHTML = '<span>' +  element.name + '</span><span>' + "Position : " + element.position + '</span><span>' + "Owner : " + element.player_id + '</span>';
+    planetDisplay.appendChild(planet);
+
+    });
 }
 
 const formButton = document.getElementById('form-button');
-formButton.addEventListener('click', update);
+
+solarSystemChoice.addEventListener('change', update);
+galaxyChoice.addEventListener('change', update);
