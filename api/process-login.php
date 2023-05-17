af4f0e41e716212812394c1bb14a8f0c840307e6
camilbenameur <benameurcamil@gamil.com>
2023-05-17 19:26:17 +0200

    fixed redirection path

--- api/process-login.php
+++ api/process-login.php
@@ -15,11 +15,13 @@ if(isset($_POST['name']) && isset($_POST['password']) && isset($_POST['mail']))
     echo "Identifiants corrects";
     $_SESSION["connected"] = true;
     $_SESSION["universe"] = $_POST['universe-choice'];
+    $_SESSION["galaxy-choice"] = 1;
+    $_SESSION["solar-system-choice"] = 1;
     header("Location:../front/galaxy.php");
 }
  else {
     echo "Identifiants incorrects";
-    header("Location:../front/login.html");
+    header("Location:../front/login.php");
 }
 }
 else
