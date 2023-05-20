0f872d61cdad3e4adbaf0ab52bb1754a484c86a9
G3R74W <t.wendl28@gmail.com>
2023-05-20 22:32:06 +0200

    added disconnect button to galaxy.php

--- front/galaxy.php
+++ front/galaxy.php
@@ -26,36 +26,46 @@ if(!isset($_SESSION["connected"]) || $_SESSION["connected"] !== true)
     <form id="choice-form" class="choice" >
         <p>Galaxy</p>
         <select id="galaxy-choice" name="galaxy-choice" class="location-choice">
+            
             <?php
-                    $db = new PDO("mysql:host=localhost;dbname=esigalactic","root", "");
-                    $query = $db->prepare("SELECT id, name FROM galaxy WHERE ? = universe_id;");
-                    $query->execute([$_SESSION["universe"]]);
-                    $rows = $query->fetchAll();
-                    foreach($rows as $row)
-                    {
-                        echo "<option value='".$row["id"]."'>".$row["name"]."</option>";
-                    }
+                $db = new PDO("mysql:host=localhost;dbname=esigalactic","root", "");
+                $query = $db->prepare("SELECT id, name FROM galaxy WHERE ? = universe_id;");
+                $query->execute([$_SESSION["universe"]]);
+                $rows = $query->fetchAll();
+                foreach($rows as $row)
+                {
+                    echo "<option value='".$row["id"]."'>".$row["name"]."</option>";
+                }
             ?>
+            
+            
         </select>
         <p>Solar system</p>
         <select id="solar-system-choice" name="solar-system-choice" class="location-choice">    
+            
+                
             <?php 
-                    $db = new PDO("mysql:host=localhost;dbname=esigalactic","root", "");
-                    $query = $db->prepare("SELECT id, name FROM solar_system WHERE ? = galaxy_id;");
-                    $query->execute([$_SESSION["galaxy-choice"]]);
-                    $rows = $query->fetchAll();
-                    foreach($rows as $row)
-                    {
-                        echo "<option value='".$row["id"]."'>".$row["name"]."</option>";
-                    }
+                $db = new PDO("mysql:host=localhost;dbname=esigalactic","root", "");
+                $query = $db->prepare("SELECT id, name FROM solar_system WHERE ? = galaxy_id;");
+                $query->execute([$_SESSION["galaxy-choice"]]);
+                $rows = $query->fetchAll();
+                foreach($rows as $row)
+                {
+                    echo "<option value='".$row["id"]."'>".$row["name"]."</option>";
+                }
             ?>
+            
         </select>
     </form>
-    <!-- <form id="choice-form" action="../api/disconnect.php" method="post">
-        <input id="disconnect" type="submit" value="Disconnect">
-    </form> -->
+    <div class="disconnect-button">
+        <form id="choice-form" action="../api/disconnect.php" method="post">
+            <input id="disconnect" type="submit" value="Disconnect">
+        </form>
+    </div>
     <div id="planet-display" class="planet-display">   
         <script src="../js/galaxy.js"></script>    
     </div>
+
+
 </body>
 </html>
