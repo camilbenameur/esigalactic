2bdeeaee273d73585b800a073d59c2ce2d9fc70b
camilbenameur <benameurcamil@gamil.com>
2023-05-23 19:29:23 +0200

    refactoring

--- js/galaxy.js
+++ js/galaxy.js
@@ -1,26 +1,40 @@
-let url = 'http://esigalactic/api/galaxyAPI.php';
-let galaxyChoice = document.getElementById('galaxy-choice');
-let solarSystemChoice = document.getElementById('solar-system-choice');
-let planetDisplay = document.getElementById('planet-display');
-
-async function update() {
-  planetDisplay.innerHTML = '';
-  const galaxyId = galaxyChoice.value;
-  const solarSystemId = solarSystemChoice.value;
-  const answer = await fetch(url + '?galaxy-choice=' + galaxyId + '&solar-system-choice=' + solarSystemId);
-  const data = await answer.json();
-  console.log(data);
-  data.forEach(element => {
-
-    let planet = document.createElement('p');
-    planet.id = 'planet-' + element.id;
-    planet.classList.add('planet');
-    planet.innerHTML = '<span>' +  element.name + '</span><span>' + "Position : " + element.position + '</span><span>' + "Owner : " + element.player_id + '</span>';
-    planetDisplay.appendChild(planet);
-
+class GalaxyAPI {
+  constructor(url, galaxyChoiceId, solarSystemChoiceId, planetDisplayId) {
+    this.url = url;
+    this.galaxyChoice = document.getElementById(galaxyChoiceId);
+    this.solarSystemChoice = document.getElementById(solarSystemChoiceId);
+    this.planetDisplay = document.getElementById(planetDisplayId);
+    
+    this.update = this.update.bind(this);
+    this.solarSystemChoice.addEventListener('change', this.update);
+    this.galaxyChoice.addEventListener('change', this.update);
+  }
+  
+  async fetchData() {
+    this.planetDisplay.innerHTML = '';
+    const galaxyId = this.galaxyChoice.value;
+    const solarSystemId = this.solarSystemChoice.value;
+    const answer = await fetch(`${this.url}?galaxy-choice=${galaxyId}&solar-system-choice=${solarSystemId}`);
+    return answer.json();
+  }
+  
+  update() {
+    this.fetchData().then(data => {
+      console.log(data);
+      data.forEach(element => {
+        let planet = document.createElement('p');
+        planet.id = 'planet-' + element.id;
+        planet.classList.add('planet');
+        planet.innerHTML = '<span>' + element.name + '</span><span>' + "Position : " + element.position + '</span><span>' + "Owner : " + element.player_id + '</span>';
+        this.planetDisplay.appendChild(planet);
+      });
     });
+  }
+  
+  init() {
+    window.onload = this.update;
+  }
 }
 
-window.onload = update;
-solarSystemChoice.addEventListener('change', update);
-galaxyChoice.addEventListener('change', update);
+const galaxyAPI = new GalaxyAPI('http://esigalactic/api/galaxyAPI.php', 'galaxy-choice', 'solar-system-choice', 'planet-display');
+galaxyAPI.init();
