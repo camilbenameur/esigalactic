20a1e69bfdf04faf3aee4cb3ee03462fbb05d73b
Camil Benameur <benameurcamil@gmail.com>
2023-05-27 18:23:01 +0200

    added set up techs button

--- front/admin.php
+++ front/admin.php
@@ -9,16 +9,15 @@
 </head>
 <body>
     <div class="choice">
-
         <form method="post" action="../api/create-universe.php" >
             <input type="text" name="universeName" placeholder="Universe name">
             <input type="submit" value="New universe">
         </form>
-
-
         <form method="post" action="../api/set-up-infrastructures.php" >
             <input type="submit" value="set-up-infrastructures">
         </form>
-
+        <form method="post" action="../api/set-up-technologies.php">
+            <input type="submit" value="set-up-technologies">
+        </form>
     </div>
 </body>
