9cb84a488eb95f396c83626cbb118629834a3af0
Camil Benameur <benameurcamil@gmail.com>
2023-05-28 19:04:46 +0200

    now handles resources display

--- js/technology.js
+++ js/technology.js
@@ -195,6 +195,7 @@ function weaponryDisplay(weaponryTechnology, weaponryTechnologyLevel) {
 
 
 async function update() {
+    updateWalletData();
     const answ = await fetch("http://esigalactic/api/technologies-displayAPI.php");
     const data = await answ.json();
     console.log(data);
@@ -298,5 +299,21 @@ async function update() {
 
 }
 
+let metalDisplay = document.getElementById('metal-display');
+let deuteriumDisplay = document.getElementById('deuterium-display');
+let energyDisplayD = document.getElementById('energy-display');
+
+async function updateWalletData() {
+    const walletAnswer = await fetch('http://esigalactic/api/walletAPI.php');
+    const walletData = await walletAnswer.json();
+    
+    const metal = walletData[0].metal;
+    const deuterium = walletData[0].deuterium;
+    const energy = walletData[0].energy;
+
+    metalDisplay.innerHTML = "Metal : " + metal;
+    deuteriumDisplay.innerHTML = "Deuterium : " + deuterium;
+    energyDisplayD.innerHTML = "Energy : " + energy;
+}
 
 window.onload = update();
